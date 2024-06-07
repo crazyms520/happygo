812bfdf9ba4e71614cd04ec6f01903b3cee7dee1
SaSaChu <teresa7397@gmail.com>
2024-06-07 13:29:55 +0800

    開版

--- cmd/libs/node/deploy/PushNewBranch.js
+++ cmd/libs/node/deploy/PushNewBranch.js
@@ -1,9 +1,3 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
 
 const Display  = require('../Display')
 const Xterm    = require('../Xterm')
