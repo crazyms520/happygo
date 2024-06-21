ad373694344deec5c00e035f0bfa853516b1ef3d
SaSaChu <teresa7397@gmail.com>
2024-06-21 16:42:15 +0800

    更新黃金大banner＋header header＋手機版

--- js/public.js
+++ js/public.js
@@ -1,9 +1,52 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
- 
+
 $(function() {
+	// 跑馬燈
+	$('.marquee.top-marquee').slick({
+    speed: 18000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+  });
+
+	// search btn
+  document.addEventListener("touchstart", function(){}, true);
+
+  // 黃金大Banner
+	$('.center').slick({
+		autoplay: true,
+  	autoplaySpeed: 2000,
+	  centerMode: true,
+	  centerPadding: '0',
+	  slidesToShow: 3,
+  	slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
 });
