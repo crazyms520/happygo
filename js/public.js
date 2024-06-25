91903749079f2497a24d0ce4c3d56288a3a80f29
SaSaChu <teresa7397@gmail.com>
2024-06-25 16:06:28 +0800

    首頁開完

--- js/public.js
+++ js/public.js
@@ -20,33 +20,57 @@ $(function() {
   document.addEventListener("touchstart", function(){}, true);
 
   // 黃金大Banner
-	$('.center').slick({
-		autoplay: true,
-  	autoplaySpeed: 2000,
-	  centerMode: true,
-	  centerPadding: '0',
-	  slidesToShow: 3,
-  	slidesToScroll: 1,
-	  responsive: [
-	    {
-	      breakpoint: 768,
-	      settings: {
-	        arrows: false,
-	        centerMode: true,
-	        centerPadding: '40px',
-	        slidesToShow: 3
-	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        arrows: false,
-	        centerMode: true,
-	        centerPadding: '40px',
-	        slidesToShow: 1
-	      }
-	    }
-	  ]
-	});
+  var swiper = new Swiper(".mySwiper", {
+  	autoplay: {
+   	delay: 3000,
+ 		},
+    slidesPerView: 3,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+  });
+	// $('.center').slick({
+	// 	// autoplay: true,
+  // 	// autoplaySpeed: 2000,
+	//   centerMode: true,
+	//   centerPadding: '0',
+	//   slidesToShow: 3,
+  // 	slidesToScroll: 1,
+	//   responsive: [
+	//     {
+	//       breakpoint: 768,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 1
+	//       }
+	//     },
+	//     {
+	//       breakpoint: 480,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 1
+	//       }
+	//     }
+	//   ]
+	// });
 
 });
